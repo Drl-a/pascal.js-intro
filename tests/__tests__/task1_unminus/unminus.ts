745fe840713359e40d0e24ce5156e2674f7519a7
Drl <Dariyachs@yandex.ru>
2025-04-17 10:01:59 +0300

    поддержка нескольких минусов

--- tests/__tests__/task1_unminus/unminus.ts
+++ tests/__tests__/task1_unminus/unminus.ts
@@ -13,7 +13,7 @@ let pjs = runFile(import.meta.url, 'unminus.code');
 test('result = -1', () => {
   expect(pjs.engine.results[0]).toBe(-1);
 });
-/** 
+
 test('result = -2', () => {
   expect(pjs.engine.results[1]).toBe(-2);
 });
@@ -37,4 +37,11 @@ test('result = 20', () => {
 test('result = -5', () => {
   expect(pjs.engine.results[6]).toBe(-5);
 });
-*/
+
+test('result = 1', () => {
+  expect(pjs.engine.results[7]).toBe(1);
+});
+
+test('result = -1', () => {
+  expect(pjs.engine.results[8]).toBe(-1);
+});
